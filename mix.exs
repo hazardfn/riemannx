982ab6e001148f141eafdbe64f6d379811026b50
Howard Beard-Marlowe <howardb@se1-devel-162.a.snowmen.se>
2017-09-28 11:08:05 +0200

    Bump mix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Riemannx.Mixfile do
   def project do
     [
       app: :riemannx,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps(),
