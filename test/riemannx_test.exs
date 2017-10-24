6f6ce02004ed0712815d105b7c4d5946863ef9e7
Howard Beard-Marlowe <howardb@Christoffers-MacBook-Pro.local>
2017-10-24 23:56:00 +0200

    [ISSUE-12] Be smart about adding host data

--- test/riemannx_test.exs
+++ test/riemannx_test.exs
@@ -1,6 +1,7 @@
 defmodule RiemannxTest do
   use ExUnit.Case, async: false
   alias Riemannx.Errors.InvalidMetricError
+  alias Riemannx.Proto.Event
   alias Riemannx.Proto.Msg
 
   test "Invalid events trigger an error" do
@@ -12,5 +13,21 @@ defmodule RiemannxTest do
   test "Query return values" do
     assert Msg.decode(Riemannx.Connection.query_ok()) == Msg.new(ok: true)
     assert Msg.decode(Riemannx.Connection.query_failed()) == Msg.new(ok: false)
+  end
+
+  test "Setting a host name returns an event with that host" do
+    event = [
+      service: "riemannx-elixir",
+      metric: 1,
+      attributes: [a: 1],
+      description: "test"
+    ]
+    Application.put_env(:riemannx, :event_host, "test_host")
+    host = event
+    |> Event.list_to_events()
+    |> hd()
+    |> Map.get(:host, nil)
+
+    assert("test_host" == host)
   end
 end
